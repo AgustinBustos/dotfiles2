d8abc22382246883042d25b96dc3648726a0ac50
Agustin Bustos Barton <88463929+AgustinBustos@users.noreply.github.com>
2025-06-24 17:43:12 -0300

    Add files via upload

--- scripts/pywalscrp.sh
+++ scripts/pywalscrp.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-wal -i ~/Pictures/Wallpapers/ --saturate 0.9
-#wpg -a ~/Pictures/Wallpapers
+#wal -a 100 -i ~/Pictures/Wallpapers --saturate 0.99 
+#wal -a "alpha" --saturate 0.99  -i ~/Pictures/Wallpapers 
+wal -a 0.99 --saturate 0.99  -i ~/Pictures/Wallpapers 
+
 # -a 92
+# -a 100 
+wpg -a ~/Pictures/Wallpapers/
 feh --bg-max "$(< "${HOME}/.cache/wal/wal")"
 python3 /home/agus/.config/rofi/colors/create_color_theme.py
 #bash ~/scripts/thor-colors.sh
